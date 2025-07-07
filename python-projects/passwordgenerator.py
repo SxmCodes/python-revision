697f2c5d1301ae557ccf45598c538c9b65b1b215
Saksham <sakshamsharma1029@gmail.com>
2025-07-07 20:16:17 +0530

    Python Day-3

--- python-projects/passwordgenerator.py
+++ python-projects/passwordgenerator.py
@@ -1,5 +1,6 @@
 import random 
 import string
+from cryptography.fernet import Fernet
 
 def getuserdetails():
     """Get user details for password generation."""
@@ -34,6 +35,20 @@ if not charsets:
 # join everything!
 mainpassword = ''.join(random.choice(charsets) for _ in range(length))
 print(f"Generated password: {mainpassword}")
+
+# saving the password to a file
+with open("password.txt", "w") as file:
+    file.write(mainpassword)
+    print("Password saved to password.txt")
+
+key = Fernet.generate_key()  # Generate a key for encryption.
+f = Fernet(key)  # Create a Fernet object with the key.
+# encrypted_password = f.encrypt(mainpassword.encode())  # Encrypt the password.
+# Saving it to file. 
+with open("password.txt", "wb") as file:
+    file.write(f.encrypt(mainpassword.encode()))  # Write the encrypted password to the file.
+    print("Password saved to password.txt")
+
 
 if __name__ == "__main__":
     # This allows the script to be run directly.
